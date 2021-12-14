9b3449cd038c29ec6c40004200c930327576dba8
Atsushi Kawakubo <63512586+Kawakubo-A@users.noreply.github.com>
2021-12-15 08:44:23 +0900

    update 2-12

--- django_app/hellow/views.py
+++ django_app/hellow/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request,id,nickname):
-    result = "Your id:"+str(id)+" , name:"+nickname
+#http://localhost:8000/hellow/my_name_is_taro-yamada.I_am_39_years_old.
+def index(request,nickname,age):
+    result = "Your age:"+str(age)+" , name:"+nickname
     return HttpResponse(result)
