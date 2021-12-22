428249df149509e9ded0173348165716f3dcc83b
Atsushi Kawakubo <63512586+Kawakubo-A@users.noreply.github.com>
2021-12-22 09:53:40 +0900

    update django

--- django_app/hellow/views.py
+++ django_app/hellow/views.py
@@ -4,6 +4,19 @@ from django.http import HttpResponse
 # Create your views here.
 #http://localhost:8000/hellow/my_name_is_taro-yamada.I_am_39_years_old.
 def index(request):
+    params={
+        'title':'Hello/Index',
+        'msg':'これはサンプルです',
+        'goto':'next',
+    }
     # result = "Your age:"+str(age)+" , name:"+nickname
     # return HttpResponse(result)
-    return render(request, 'hellow/index.html')
+    return render(request, 'hellow/index.html',params)
+
+def next(request):
+    params={
+        'title':'Hello/Next',
+        'msg':'これはサンプルです',
+        'goto':'index',
+    }
+    return render(request, 'hellow/index.html',params)
