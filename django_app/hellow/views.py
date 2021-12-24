51e29070749df488ce553212d38c453149b6445f
Atsushi Kawakubo <63512586+Kawakubo-A@users.noreply.github.com>
2021-12-24 09:27:12 +0900

    update 2-31,32

--- django_app/hellow/views.py
+++ django_app/hellow/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import HelloForm
 
 # Create your views here.
 #http://localhost:8000/hellow/my_name_is_taro-yamada.I_am_39_years_old.
 def index(request):
     params = {
         'title':'Hello/Index',
-        'msg':'お名前は？',
+        'message':'Your data',
+        'form':HelloForm(),
     }
+    if(request.method=='POST'):
+        params['message']='名前'+request.POST['name'] + \
+            '<br> メール:'+request.POST['mail'] + \
+            '<br> 年齢:'+request.POST['age']
+        params['form']=HelloForm(request.POST)
     return render(request, 'hellow/index.html', params)
 
 def form(request):
